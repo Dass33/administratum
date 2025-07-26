ae10009ee2b9bffc2e4fc567f583630bb4f7f898
Dass33 <danieldvoraku@gmail.com>
2025-07-26 09:12:28 +0200

    add dropdown to save button

--- frontend/src/saveButton.tsx
+++ frontend/src/saveButton.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import dropdownArrow from "./assets/dropdown_arrow.svg";
 
 function SaveButton() {
@@ -7,11 +8,47 @@ function SaveButton() {
                 <span className="text-2xl border-figma-white ml-3">Save</span>
             </button>
 
-            <button className="hover:bg-green-700 px-2 h-full rounded-e-lg">
-                <img className="pt-1" src={dropdownArrow} alt="dropdown arrow" />
-            </button>
+            <SaveDropdown />
         </div>
     );
 }
 
 export default SaveButton;
+
+
+const SaveDropdown = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const options = ["Export", "Share"]
+
+    const handleSelect = (option: string): void => {
+        console.log(option);
+        setIsOpen(false);
+    };
+
+    return (
+        <div className="relative inline-block text-left h-full">
+            <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="hover:bg-green-700 px-2 h-full rounded-e-lg"
+            >
+                <img className="pt-1" src={dropdownArrow} alt="dropdown arrow" />
+            </button>
+
+            {isOpen && (
+                <div className="absolute z-10 my-1 w-28 right-0 bg-white border border-gray-300 rounded-md shadow-lg">
+                    <ul className="max-h-60 overflow-auto py-1">
+                        {options.map((option, index) => (
+                            <li
+                                key={option || index}
+                                onClick={() => handleSelect(option)}
+                                className="px-4 py-2 text-sm text-gray-700 hover:bg-blue-50 hover:text-blue-700 cursor-pointer"
+                            >
+                                {option}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+};
