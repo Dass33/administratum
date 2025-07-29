d84ecc75b5418d14a434984e3c08483fc4ddc889
Dass33 <danieldvoraku@gmail.com>
2025-07-29 09:46:24 +0200

    prepare login page for connection

--- frontend/src/auth.tsx
+++ frontend/src/auth.tsx
@@ -1,50 +1,64 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useApp } from './AppContext';
 import logo from "./assets/logo.svg";
 
+type loginData = {
+    email: string;
+    password: string;
+};
+
 const Auth = () => {
     const {
-        setSettingsModal,
-        gameUrl, setGameUrl
+        setAuthenticated
     } = useApp();
     const stopPropagation = (e: React.MouseEvent) => {
         e.stopPropagation();
     };
 
-    const [newGameUrl, setNewGameUrl] = useState(gameUrl);
+    const [email, setEmail] = useState<string>();
+    const [password, setPassword] = useState<string>();
+    const [error, setError] = useState<string | null>(null);
 
-    const saveAndExit = () => {
-        setGameUrl(newGameUrl);
-        setSettingsModal(false);
-    }
+    const handleSubmit = async (data: loginData) => {
+        try {
+            const response = await fetch('http://localhost:8080/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            });
 
-    useEffect(() => {
-        const handleKeyDown = (e: KeyboardEvent) => {
-            if (e.key === 'Escape') saveAndExit()
-        };
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
 
-        document.addEventListener('keydown', handleKeyDown);
-        return () => {
-            document.removeEventListener('keydown', handleKeyDown);
-        };
-    }, [setSettingsModal]);
+            const result = await response.json();
+            setError(null);
+            console.log('Success:', result);
+            setAuthenticated(true);
+
+        } catch (err) {
+            setError(err instanceof Error ? err.message : 'An error occurred');
+        }
+    };
 
     return (
         <div
             className="fixed inset-0 bg-black bg-opacity-15 flex justify-center items-center z-50"
-            onClick={saveAndExit}
         >
             <div onClick={stopPropagation}>
                 <div className="p-4 rounded-lg text-figma-black bg-figma-white font-medium
-                    overflow-y-auto focus:outline-none w-[22rem]">
+                    overflow-y-auto focus:outline-none w-[22rem] mb-16">
                     <div className='flex items-center mb-4 gap-4'>
                         <img className='size-12 border-2 bg-figma-white border-figma-black p-1 rounded-lg' src={logo} />
                         <h2 className='text-2xl text-figma-black'>Login</h2>
                     </div>
                     <div className=''>
                         <input className="w-80 border border-figma-gray bg-figma-white focus:outline-none rounded-lg p-2"
+                            type='email'
                             placeholder="Email"
-                            onChange={(e) => { setNewGameUrl(e.target.value) }}
+                            onChange={(e) => { setEmail(e.target.value) }}
                         />
                     </div>
 
@@ -52,16 +66,23 @@ const Auth = () => {
                         <input className="w-80 border border-figma-gray bg-figma-white focus:outline-none rounded-lg p-2"
                             type='password'
                             placeholder="Password"
-                            onChange={(e) => { setNewGameUrl(e.target.value) }}
+                            onChange={(e) => { setPassword(e.target.value) }}
                         />
                     </div>
                     {/*<button className='rounded-lg mt-2 text-xs underline ml-1 text-figma-black'>
                         Reset password
                     </button>*/}
+                    {error && <span className='text-red-600 text-xs ml-1'>Incorrect credentials</span>}
 
 
-                    <div className='flex gap-4 justify-between  mt-5'>
-                        <button className='bg-figma-green rounded-lg p-2 px-4 text-figma-white font-bold'>
+                    <div className={`flex gap-4 justify-between ${error ? "mt-2" : "mt-5"}`}>
+                        <button className='bg-figma-green rounded-lg p-2 px-4 text-figma-white font-bold'
+                            onClick={() => {
+                                if (email && password) {
+                                    handleSubmit({ email: email, password: password })
+                                }
+                            }}
+                        >
                             Login
                         </button>
                         <button className='rounded-lg p-2 px-4 text-figma-black'>
