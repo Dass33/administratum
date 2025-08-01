9b6b3d4e837b445bb37f9711822792bdd58cd65f
Dass33 <danieldvoraku@gmail.com>
2025-08-01 09:54:08 +0200

    display recieved sheet names

--- frontend/src/bottomBar.tsx
+++ frontend/src/bottomBar.tsx
@@ -1,42 +1,16 @@
-import { useEffect, useState } from "react";
 import settings from "./assets/settings.svg"
 import Dropdown from "./dropdown";
-import { useApp, CurrSheet, ColSuffix, Sheets } from "./AppContext";
-import plus from "./assets/plus.svg"
+import { useApp, CurrSheet, ColSuffix } from "./AppContext";
 
 const BottomBar = () => {
     const {
-        sheets, setSheets,
         currSheet, setCurrSheet,
         setCurrTable,
         columns, setColumns,
         setSheetModal,
         setSettingsModal,
+        loginData,
     } = useApp();
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        const stored = localStorage.getItem(Sheets);
-        console.log(stored);
-        if (stored) {
-            setSheets(JSON.parse(stored))
-            setLoading(false);
-            return
-        }
-
-        fetch('http://localhost:8080/sheets')
-            .then(response => response.json())
-            .then((data: string[]) => {
-                setSheets(data);
-                if (!currSheet) setCurrSheet(data[0])
-                setLoading(false);
-            })
-            .catch(error => {
-                setError(error);
-                setLoading(false);
-            });
-    }, []);
 
     return (
         <div className="flex flex-row gap-4 items-center">
@@ -45,29 +19,31 @@ const BottomBar = () => {
             >
                 <img className="" src={settings} />
             </button>
-            {(!loading && !error) &&
-                <Dropdown
-                    options={sheets.map(item => {
-                        return { value: item, label: item }
-                    })}
-                    placeholder={currSheet}
-                    onSelect={(item) => {
-                        localStorage.setItem(currSheet + ColSuffix, JSON.stringify(columns));
-                        setCurrSheet(item.value)
-                        localStorage.setItem(CurrSheet, item.value);
-                        setColumns(() => {
-                            const stored = localStorage.getItem(item.value + ColSuffix);
-                            return stored ? JSON.parse(stored) : [];
-                        })
-                        setCurrTable(() => {
-                            const stored = localStorage.getItem(item.value);
-                            return stored ? JSON.parse(stored) : [];
-                        });
-                    }}
-                    isDown={false}
-                    addNewValue={() => setSheetModal(true)}
-                />
-            }
+            <Dropdown
+                options={(loginData?.opened_sheet?.sheets_id_names ?? []).map(item => ({
+                    value: item.id,
+                    label: item.name
+                }))}
+                placeholder={loginData?.opened_sheet?.name != ""
+                    ? loginData?.opened_sheet?.name
+                    : "Sheets"
+                }
+                onSelect={(item) => {
+                    localStorage.setItem(currSheet + ColSuffix, JSON.stringify(columns));
+                    setCurrSheet(item.value)
+                    localStorage.setItem(CurrSheet, item.value);
+                    setColumns(() => {
+                        const stored = localStorage.getItem(item.value + ColSuffix);
+                        return stored ? JSON.parse(stored) : [];
+                    })
+                    setCurrTable(() => {
+                        const stored = localStorage.getItem(item.value);
+                        return stored ? JSON.parse(stored) : [];
+                    });
+                }}
+                isDown={false}
+                addNewValue={() => setSheetModal(true)}
+            />
         </div>
     );
 }
